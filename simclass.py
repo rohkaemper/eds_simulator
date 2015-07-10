0d7fe4b0495191993e622fd5bb72a994b33e2e65
Sebastian Rohkaemper <sebastian.rohkaemper@uni-due.de>
2015-07-10 19:15:13 +0200

    Refactored Code. Implemented Class for Simulation. Logging to csv file in Folder logs.
    Clear and more readable main. Class for simulated source/process system.
    Logging to csv-Files added.
    Evaluation and plotting of data temporarily removed.

---
+++ simclass.py
@@ -0,0 +1,100 @@
+""" Simulation object """
+import utils
+import logging
+import pprint
+import decimal
+
+
+class sim_queue(object):
+
+    def __init__(self, env, res, config, seed, capacity):
+        self.env = env
+        self.res = res
+        self.seed_conf = int(seed)
+        self.capacity = int(config['capacity'])
+        self.interarrival_rate = int(config['avg_interarrival_time'])
+        self.service_duration = int(config['service_time'])
+        self.avg_patience = config['avg_patience']
+        self.generators = utils.read_generators('./opt/generator.csv')
+        self.interarrival_generator = self.generators[
+            int(config['interarrival_gen'])]
+        self.service_time_generator = self.generators[
+            int(config['service_time_gen'])]
+        self.patience_generator = self.generators[int(config['patience_gen'])]
+        self.generated_processes = 0
+        self.log = []
+        logging.info('Source object initialized')
+
+        # This generates a process of instantiated object
+        env.process(self.run())
+
+    def run(self):
+        """ The source is responsible to generate processeses as long as the simulation is active """
+
+        while(True):
+            # generate time for next process and it's service time
+            next_event = self.interarrival_generator(self.interarrival_rate)
+            process_service_time = self.service_time_generator(
+                self.service_duration)
+            process_patience = .0
+            if (self.avg_patience.isdigit() and (float(self.avg_patience) > .0)):
+                process_patience = self.patience_generator(float(self.avg_patience))
+            elif (self.avg_patience == 'max'):
+                process_patience = float('inf')
+            else:
+                process_patience = 0
+            assert process_patience >= 0, 'Value must be positive!'
+            process_patience = max(process_patience, 0)
+            yield self.env.timeout(next_event)
+            # generate new process
+            config = self.env, self.res, process_service_time, process_patience
+            NewProcess = Process(config, self.log)
+            self.env.process(NewProcess.run())
+#            self.WriteLog(config, 0)
+
+    def WriteLog(self, config, toFile):
+        if (toFile):
+            pass
+        else:
+            logging.info(
+                'Running with configuration:\n' + pprint.pformat(config))
+            logging.info(self.log)
+
+
+class Process(object):
+
+    def __init__(self, config, log):
+        env, res, service_time, process_patience = config
+        self.env = env
+        self.res = res
+        self.entered_system = self.env.now
+        self.wait = .0
+        self.service_time = service_time
+        self.process_patience = process_patience
+        self.dropped_out = False
+        self.time_in_system = .0
+
+        self.log = log
+        logging.debug('Process object generated.')
+
+    def run(self):
+        self.res_usage_in = int(self.res.count)
+        with self.res.request() as req:
+            result = yield req | self.env.timeout(self.process_patience)
+
+            self.wait = self.env.now - self.entered_system
+            if req not in result:
+                self.drop_out()
+            else:
+                yield self.env.timeout(self.service_time)
+                self.time_in_system = self.env.now - self.entered_system
+                logging.debug(
+                    'Process has been served [%.2f->%.2f].' % (self.entered_system, self.env.now))
+
+        self.res_usage_out = int(self.res.count)
+        self.log.append([self.entered_system, self.wait, self.service_time, self.time_in_system, self.dropped_out, self.res_usage_in, self.res_usage_out])
+
+    def drop_out(self):
+        self.time_in_system = (self.env.now - self.entered_system)
+        self.dropped_out = True
+        logging.debug('Process dropped out at %.2f.')
--- simmain.py
+++ simmain.py
@@ -1,121 +1,135 @@
 import simpy
 import random
 import logging
-import numpy as np
+import csv
+# import numpy as np
 import sys
+import os
 import matplotlib
+import datetime
 
-from collections import defaultdict
+# from collections import defaultdict
 from utils import *
-from processes import *
+from simclass import *
+# from processes import *
 
 LOG = 'INFO'
 avg_wait = .0
 drop_probability = .0
 
+
 def main():
     """ Read configuration file, set parameters and start simulation """
     # Set logging level:
     if(LOG == 'DEBUG'):
-        logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+        logging.basicConfig(
+            level=logging.DEBUG, format='%(levelname)s: %(message)s')
     elif (LOG == 'INFO'):
-        logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+        logging.basicConfig(
+            level=logging.INFO, format='%(levelname)s: %(message)s')
     else:
-        logging.basicConfig(level=logging.WARN, format='%(levelname)s: %(message)s')
+        logging.basicConfig(
+            level=logging.WARN, format='%(levelname)s: %(message)s')
 
     logging.info('Start of Simulation')
     # read config file; try catch block... for missing file
     logging.info('reading config file...')
     config = readConfig('./opt/sim.cfg', 'default')
-    avg_interarrival = int(config['avg_interarrival_time'])
-    service_time = int(config['service_time'])
-
-    logging.debug(config)
-    # TODO: this needs to be implemented yet
-    adv_config = readConfig('./opt/sim.cfg', 'advanced')
-    logging.info(adv_config)
-
-    logging.info('setting parameters...')
-    # set values according to sim.cfg
-    simulation_duration = config['simulation_duration']
-    num_runs = int(adv_config['number_of_repetitions_per_simulation'])
-    num_sims = int(adv_config['number_of_simulations'])
-    alter_capacity_by = int(adv_config['alter_capacity_by'])
-    seed = []
-    if (config['seed'] == 'rand'):
-        for i in xrange(0,num_runs):
-            seed.append(random.randint(0, sys.maxint))
-        logging.warning('Randomized initial seed used for measurements (seed = %s)!' % seed)
-
-    else:
-        seed.append(int(config['seed']))
-        logging.info('Single Seed configured -> enforcing single simulation run with seed %d.' % seed[0])
-        # offset of 1
-        num_runs = 1
-
-    # initialize numpy array with zeros
-    data = [.0, .0, .0]
-
-    avg_over_runs = [.0, .0, .0]
-    cap = []
-    # !!! you have to put an int into capacity, or it will not work !!!
-    number_of_queues = int(config['capacity'])
-    simlog = []
-    runlog = []
-    res_usage = []
-    drop_log = []
-    data_log = []
-    logging.info('Number of Simulations: %d \n Number of runs: %d' % (num_sims, num_runs))
-    for sim_run in range(0,num_sims):
-        logging.info('--- Simulation %2d ---' % (sim_run+1))
-        actual_capacity = number_of_queues + (sim_run*alter_capacity_by)
-        for repetition in xrange(0, num_runs):
-            cap.append(actual_capacity)
-            data = [.0, .0, .0]
-            res_usage = []
-            drop_at = []
-            logging.debug('Setting seed to: %d' % (seed[repetition]))
-            random.seed(int(seed[repetition]))
-            # initialize environment and resource
-            env = simpy.Environment()
-            res = simpy.Resource(env, capacity=actual_capacity)
-            # create processes
-            s = env.process(Source(env, res, data, res_usage, drop_at))
-            logging.debug('%10.1f: start' % env.now)
-
-            env.run(until=int(simulation_duration))
-            logging.debug('%10.1f: end' % env.now)
-            logging.info('Queue capacity:\t %d' % (res._capacity))
-            logging.info('Simulation (%d) ended with: \n\t avg. waiting time \t= %.2ftu \n\t drop_probability \t= %.2f%%' % (repetition+1, data[0], 100*data[1]))
-            # logging.info('evaluating and displaying data')
-            avg_over_runs[0] = (avg_over_runs[0] + data[0]) / 2
-            avg_over_runs[1] = (avg_over_runs[1] + data[1]) / 2
-            avg_over_runs[2] = (avg_over_runs[2] + data[2]) / 2
-            # logs data of current run
-            runlog.append(data)
-            data_log.append(res_usage)
-            drop_log.append(drop_at)
-            del res_usage
-            del data
-            del drop_at
-
-        logging.debug('Appending current evaluation to list -> %s' % (avg_over_runs))
-        simlog.append(avg_over_runs)
-        avg_over_runs = [.0, .0, .0]
-
-    logging.debug('Resource usage: %s \n %d %d' % (data_log, num_sims, num_runs))
-    plotUsage(data_log, drop_log, 10, num_sims, num_runs, cap)
-
-
     logging.info('Simulation Configuration:\n\t arrival function \t\t= %s, param = %stu \n\t service time function \t= %s, param = %stu \n\t patience function \t\t= %s, param = %stu' % (
         config['interarrival_gen'], config['avg_interarrival_time'],
         config['service_time_gen'], config['service_time'],
         config['patience_gen'], config['avg_patience'])
     )
+    # avg_interarrival = int(config['avg_interarrival_time'])
+    # service_time = int(config['service_time'])
+
+    # logging.debug(config)
+    # TODO: this needs to be implemented yet
+    adv_config = readConfig('./opt/sim.cfg', 'advanced')
+    # logging.info(adv_config)
+
+    logging.info('setting parameters...')
+    # set values according to sim.cfg
+    simulation_duration = int(config['simulation_duration'])
+    num_runs = int(adv_config['number_of_repetitions_per_simulation'])
+    num_sims = int(adv_config['number_of_simulations'])
+    alter_capacity_by = int(adv_config['alter_capacity_by'])
+    seed = ConfigureSeeds(config, num_runs)
+    data = []
+    cap = []
+    # !!! you have to put an int into capacity, or it will not work !!!
+    number_of_queues = int(config['capacity'])
+
+    logging.info('Number of Simulations: %d \n Number of runs: %d' %
+                 (num_sims, num_runs))
+
+    # START of SIMULATION
+    for sim_run in range(0, num_sims):
+        logging.info('--- Simulation %2d ---' % (sim_run + 1))
+        actual_capacity = number_of_queues + (sim_run * alter_capacity_by)
+
+        for repetition in xrange(0, len(seed)):
+            cap.append(actual_capacity)
+            logging.debug('Setting seed to: %d' % (seed[repetition]))
+            random.seed(int(seed[repetition]))
+
+            # initialize environment and resource
+            env = simpy.Environment()
+            res = simpy.Resource(env, capacity=actual_capacity)
+            # creating queue object
+            queue = sim_queue(env, res, config, 42, 1)
+            # starting simulation for ''' configured duration '''
+            env.run(until=simulation_duration)
+            logging.info(queue.log)
+            LogToCSV('./logs/', 'simlog%.2d-%.2d.csv' % (sim_run+1, repetition+1), queue.log)
+            # logging.info('Queue capacity:\t %d' % (res._capacity))
+
+
+            # logs data of current run
+
+            # delete old queue object
+            del queue
+            #
+        #
+    # END of SIMULATION
+
+    # All simulations are finished... calculate and display data!
+    # plotUsage(data_log, drop_log, 10, num_sims, num_runs, cap)
+
     # TODO: Muss angepasst werden an simrun -> Auswertung von "simlog"
     # logging.info('Final result after %d runs: \n\t avg. waiting time \t= %.2ftu \n\t drop_probability \t= %.2f%%' % (num_runs-1, avg_over_runs[0], 100*avg_over_runs[1]))
-    logging.info('Averages over runs: %s' % (runlog))
-    logging.info('Overall averages: %s' % (simlog))
+    # logging.info('Averages over runs: %s' % (runlog))
+    # logging.info('Overall averages: %s' % (simlog))
+
+
+def ConfigureSeeds(config, runs):
+    seed = []
+
+    if (config['seed'] == 'rand'):
+        for i in xrange(0, runs):
+            seed.append(random.randint(0, sys.maxint))
+        logging.info(
+        'Randomized initial seed used for measurements (seeds = %s)!' % seed)
+    else:
+        seed.append(int(config['seed']))
+        logging.info(
+            'Single Seed configured -> enforcing single simulation run with seed %d.' % seed[0])
+        # offset of 1
+
+    return seed
+
+
+def LogToCSV(dir, file, log):
+    now = str(datetime.datetime.now().strftime('%Y%m%d_%H-%M'))
+    dir += now
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    with open(os.path.join(dir,file), 'wb') as csvout:
+        writer = csv.writer(csvout)
+        writer.writerows([['t_in', 'waited_for', 'got_served_for', 'time_in_sys', 'got_dropped', 'in_res_usage', 'out_res_usage']])
+        writer.writerows(log)
+        csvout.close()
 
 if (__name__ == '__main__'):
     main()
